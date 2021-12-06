c14fcd30462ae55e42b6340d153c59f9e63f0f90
Lucio Flavio Fortunato <luciofortunato1985@gmail.com>
2021-12-06 14:48:17 -0400

    implementado o carrinho de compras 100% funcional

---
+++ saudefood/src/main/java/br/com/saudefood/domain/pedido/ItemPedido.java
@@ -0,0 +1,30 @@
+package br.com.saudefood.domain.pedido;
+
+import java.math.BigDecimal;
+
+import br.com.saudefood.domain.restaurante.ItemCardapio;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ItemPedido {
+	
+	@EqualsAndHashCode.Include
+	private Integer id;
+	
+	private ItemCardapio itemCardapio;
+	
+	private Integer quantidade;
+	
+	private String observacoes;
+	
+	private BigDecimal preco;
+	
+	public BigDecimal getPrecoCalculado() {
+		return preco.multiply(BigDecimal.valueOf(quantidade));
+	}
+	
+}
---
+++ saudefood/src/main/java/br/com/saudefood/domain/pedido/RestauranteDiferenteException.java
@@ -0,0 +1,17 @@
+package br.com.saudefood.domain.pedido;
+
+@SuppressWarnings("serial")
+public class RestauranteDiferenteException extends Exception {
+
+	public RestauranteDiferenteException() {
+		super();
+		
+	}
+
+	public RestauranteDiferenteException(String message) {
+		super(message);
+		
+	}
+	
+
+}
